6eeb04d19257e340bc2b3a7dfec6d46b55f14b3a
Arindam Basu <arindam4all@gmail.com>
2020-10-25 09:57:58 -0400

    J1-12-Order of Operations

--- src/com/codewithbasu/Main.java
+++ src/com/codewithbasu/Main.java
@@ -1,8 +1,13 @@
 package com.codewithbasu;
 
+
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        int x = 10 + 12 % 5;
+        
+        System.out.println(x);
     }
 }
